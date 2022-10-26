4fbdb3329ea110df8617ed9fa76b2c69ef71804a
matteoga95 <gallo.work95@gmail.com>
2022-10-26 14:43:25 +0200

    h1 aggiunto tramite data

---
+++ assets/js/main.js
@@ -0,0 +1,11 @@
+// Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
+
+const { createApp } = Vue;
+
+createApp({
+    data() {
+        return {          
+            message: "messaggio di benvenuto"          
+        }
+    }
+}).mount('#app');
